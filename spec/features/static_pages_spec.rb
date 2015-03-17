71b8917b1f7589d604ad5c6a1001c996c6466f6d
Yun Nan <yunnan0317@gmail.com>
2015-03-17 17:44:57 +0800

    Remove ./spec/request

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -4,10 +4,10 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    #it "should have the content 'Sample App'" do
+     # visit '/static_pages/home'
+      #expect(page).to have_content('Sample App')
+    #end
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
@@ -38,6 +38,14 @@ describe "Static pages" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the title Contact" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+      expect(page).to have_title("Ruby on Rails Tutorial Sample APP | Contact")
     end
   end
 end
